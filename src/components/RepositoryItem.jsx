ff5d46fd7f90e3ddfe1f9cf1e4fbe801e89f8314
Diego Fernandes <diego.schell.f@gmail.com>
2021-01-14 16:55:24 -0300

    Listando repositorios

--- src/components/RepositoryItem.jsx
+++ src/components/RepositoryItem.jsx
@@ -1,10 +1,10 @@
 export function RepositoryItem(props) {
   return (
     <li>
-      <strong>{props.repository.name ?? 'Default'}</strong>
+      <strong>{props.repository.name}</strong>
       <p>{props.repository.description}</p>
 
-      <a href={props.repository.link}>
+      <a href={props.repository.html_url}>
         Acessar repositório
       </a>
     </li>
--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -2,13 +2,6 @@ import { useState, useEffect } from 'react';
 import { RepositoryItem } from "./RepositoryItem";
 
 import '../styles/repositories.scss';
-
-const repository = {
-  name: 'unform',
-  description: 'Forms in React',
-  link: 'https://github.com/unform/unform'
-}
-
 export function RepositoryList() {
   const [repositories, setRepositories] = useState([]);
 
@@ -23,10 +16,9 @@ export function RepositoryList() {
       <h1>Lista de repositórios</h1>
 
       <ul>
-        <RepositoryItem repository={repository} />
-        <RepositoryItem repository={repository} />
-        <RepositoryItem repository={repository} />
-        <RepositoryItem repository={repository} />
+        {repositories.map(repository => {
+          return <RepositoryItem key={repository.name} repository={repository} />
+        })}
       </ul>
     </section>
   );
