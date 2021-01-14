5fdfe0c19ad1bae29488c5b0803b69737d966fbf
Diego Fernandes <diego.schell.f@gmail.com>
2021-01-14 16:36:36 -0300

    Estilizaçao da Listagem

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,9 @@
-import { Counter } from './components/Counter';
 import { RepositoryList } from './components/RepositoryList';
 
 import './styles/global.scss';
 
 export function App() {
   return (
-    <>
-      <RepositoryList />
-      <Counter />
-    </>
+    <RepositoryList />
   )
 }
--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,5 +1,7 @@
 import { RepositoryItem } from "./RepositoryItem";
 
+import '../styles/repositories.scss';
+
 const repository = {
   name: 'unform',
   description: 'Forms in React',
