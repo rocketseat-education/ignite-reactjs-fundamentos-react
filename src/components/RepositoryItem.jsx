60ad0a5decb0342500848b675a384c709a45020f
Diego Fernandes <diego.schell.f@gmail.com>
2021-01-14 15:02:05 -0300

    Propriedades no React

---
+++ src/components/RepositoryItem.jsx
@@ -0,0 +1,12 @@
+export function RepositoryItem(props) {
+  return (
+    <li>
+      <strong>{props.repository.name ?? 'Default'}</strong>
+      <p>{props.repository.description}</p>
+
+      <a href={props.repository.link}>
+        Acessar repositório
+      </a>
+    </li>
+  );
+}
--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,4 +1,10 @@
-const repositoryName = 'unform2';
+import { RepositoryItem } from "./RepositoryItem";
+
+const repository = {
+  name: 'unform',
+  description: 'Forms in React',
+  link: 'https://github.com/unform/unform'
+}
 
 export function RepositoryList() {
   return (
@@ -6,32 +12,10 @@ export function RepositoryList() {
       <h1>Lista de repositórios</h1>
 
       <ul>
-        <li>
-          <strong>{repositoryName}</strong>
-          <p>Forms in React</p>
-
-          <a href="">
-            Acessar repositório
-          </a>
-        </li>
-
-        <li>
-          <strong>unform</strong>
-          <p>Forms in React</p>
-
-          <a href="">
-            Acessar repositório
-          </a>
-        </li>
-
-        <li>
-          <strong>unform</strong>
-          <p>Forms in React</p>
-
-          <a href="">
-            Acessar repositório
-          </a>
-        </li>
+        <RepositoryItem repository={repository} />
+        <RepositoryItem repository={repository} />
+        <RepositoryItem repository={repository} />
+        <RepositoryItem repository={repository} />
       </ul>
     </section>
   );
