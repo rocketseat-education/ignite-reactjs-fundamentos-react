98f24eb5b58a0aa7e5b203db2a40af6aaf88a066
Diego Fernandes <diego.schell.f@gmail.com>
2021-01-14 14:19:46 -0300

    Webpack Dev Server

--- webpack.config.js
+++ webpack.config.js
@@ -11,6 +11,9 @@ module.exports = {
   resolve: {
     extensions: ['.js', '.jsx'],
   },
+  devServer: {
+    contentBase: path.resolve(__dirname, 'public'),
+  },
   plugins: [
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, 'public', 'index.html')
